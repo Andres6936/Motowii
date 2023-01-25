9027c81bcb7fbef3df32d67942570703e3f0eb8b
Andres6936 <joan.buritica@artemisa.tech>
2023-01-24 20:40:57 -0500

    Added Form

--- app/App.tsx
+++ app/App.tsx
@@ -12,26 +12,30 @@ import {
 import {useState} from "react";
 
 export default function App() {
-    const [username, setUsername] = useState<string>('');
     const [email, setEmail] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [confirmPassword, setConfirmPassword] = useState<string>('');
+    const [isEmptyUsername, setIsEmptyUsername] = useState<boolean>(false);
+    const [isEmptyEmail, setIsEmptyEmail] = useState<boolean>(false);
+    const [isEmptyPassword, setIsEmptyPassword] = useState<boolean>(false);
+    const [passwordNotMatch, setPasswordNotMatch] = useState<boolean>(false);
 
     const onHandleRegister = () => {
         if (username === "") {
-
+            setIsEmptyUsername(true);
         }
 
         if (email === "") {
-
+            setIsEmptyEmail(true);
         }
 
         if (password === "") {
-
+            setIsEmptyPassword(true);
         }
 
         if (password !== confirmPassword) {
-
+            setPasswordNotMatch(true);
         }
     }
 
@@ -39,13 +43,13 @@ export default function App() {
         <NativeBaseProvider>
             <Center flex={1}>
                 <FormControl isInvalid={true}>
-                    <Stack space={3} w={"75%"} maxW={"300px"} mx={"auto"}>
+                    <Stack space={2} w={"75%"} maxW={"300px"} mx={"auto"}>
                         <Text mb={"10"} fontSize={"2xl"}>Create an account</Text>
 
                         <Input value={username} onChangeText={text => setUsername(text)}
                                variant={"rounded"} placeholder={"Username"}></Input>
                         <FormControl.ErrorMessage
-                            ml={"5"}
+                            ml={"5"} isInvalid={isEmptyUsername}
                             leftIcon={<WarningOutlineIcon size="xs"/>}>
                             The username is empty
                         </FormControl.ErrorMessage>
@@ -54,7 +58,7 @@ export default function App() {
                                variant={"rounded"}
                                placeholder={"Email address"}></Input>
                         <FormControl.ErrorMessage
-                            ml={"5"}
+                            ml={"5"} isInvalid={isEmptyEmail}
                             leftIcon={<WarningOutlineIcon size="xs"/>}>
                             The email is empty
                         </FormControl.ErrorMessage>
@@ -64,7 +68,7 @@ export default function App() {
                                variant={"rounded"}
                                placeholder={"Password"}></Input>
                         <FormControl.ErrorMessage
-                            ml={"5"}
+                            ml={"5"} isInvalid={isEmptyPassword}
                             leftIcon={<WarningOutlineIcon size="xs"/>}>
                             The passwords is empty
                         </FormControl.ErrorMessage>
@@ -74,12 +78,12 @@ export default function App() {
                                variant={"rounded"}
                                placeholder={"Confirm Password"}></Input>
                         <FormControl.ErrorMessage
-                            ml={"5"}
+                            ml={"5"} isInvalid={passwordNotMatch}
                             leftIcon={<WarningOutlineIcon size="xs"/>}>
                             The passwords do not match
                         </FormControl.ErrorMessage>
 
-                        <Button mt={"10"}>Register</Button>
+                        <Button onPress={onHandleRegister} mt={"10"}>Register</Button>
                         <Link mx={"auto"}>Already account</Link>
                     </Stack>
                 </FormControl>
