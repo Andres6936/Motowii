f0811b953ff4328ff137b68bd3d4e2f7f10fe3d9
Andres6936 <joan.buritica@artemisa.tech>
2023-01-25 14:19:05 -0500

    Fixed Error parsing Payload

--- serveless/index.ts
+++ serveless/index.ts
@@ -5,21 +5,20 @@ import type {Event, Context, Response} from "./scaleway";
 import {ControllerMongo} from "./ControllerMongo";
 
 interface Payload {
-    TypeEvent: "CREATE_USER" | "READ_USER" | "DELETE_USER" | "UPDATE_USER",
-    ScopeEvent: "READ_ALL" | "READ_BY_ID"
+    TypeEvent: "CREATE_USER" | "READ_USER" | "UPDATE_USER" | "DELETE_USER",
+    ScopeEvent: "CREATE" | "READ_ALL" | "READ_BY_ID" | "UPDATE_BY_ID" | "DELETE_BY_ID"
 }
 
 export async function handle(event: Event, context: Context): Promise<Response> {
-    const payload = JSON.stringify(event.body) as Partial<Payload>;
+    const payload = JSON.parse(event.body) as Partial<Payload>;
 
     if (payload?.TypeEvent === "CREATE_USER") {
 
-    } else if (payload.TypeEvent === "READ_USER") {
-        const response = await ControllerMongo.readAll();
-        console.log(response, 'Response Handle')
-    } else if (payload.TypeEvent === "DELETE_USER") {
+    } else if (payload?.TypeEvent === "READ_USER") {
+        await handleRead(payload);
+    } else if (payload?.TypeEvent === "DELETE_USER") {
 
-    } else if (payload.TypeEvent === "UPDATE_USER") {
+    } else if (payload?.TypeEvent === "UPDATE_USER") {
 
     }
 
@@ -36,12 +35,20 @@ export async function handle(event: Event, context: Context): Promise<Response> 
     };
 }
 
+async function handleRead(payload: Partial<Payload>) {
+    if (payload?.ScopeEvent === "READ_ALL") {
+        const response = await ControllerMongo.readAll();
+        console.log(response, 'Response Handle')
+    }
+}
+
 
 // Handle Execution
 (async () => {
     await handle({
         body: JSON.stringify({
-            TypeEvent: "READ_USER"
+            TypeEvent: "READ_USER",
+            ScopeEvent: "READ_ALL"
         } as Payload)
     } as Event, {} as Context);
 })();
