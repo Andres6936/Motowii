bf9c20083696d77d74489a214020e63903ce29ca
Andres6936 <joan.buritica@artemisa.tech>
2023-01-24 20:35:30 -0500

    Added Form

--- app/App.tsx
+++ app/App.tsx
@@ -1,4 +1,14 @@
-import {Button, Center, Input, Link, NativeBaseProvider, Stack, Text} from "native-base";
+import {
+    Button,
+    Center,
+    FormControl,
+    Input,
+    Link,
+    NativeBaseProvider,
+    Stack,
+    Text,
+    WarningOutlineIcon
+} from "native-base";
 import {useState} from "react";
 
 export default function App() {
@@ -7,27 +17,72 @@ export default function App() {
     const [password, setPassword] = useState<string>('');
     const [confirmPassword, setConfirmPassword] = useState<string>('');
 
+    const onHandleRegister = () => {
+        if (username === "") {
+
+        }
+
+        if (email === "") {
+
+        }
+
+        if (password === "") {
+
+        }
+
+        if (password !== confirmPassword) {
+
+        }
+    }
+
     return (
         <NativeBaseProvider>
             <Center flex={1}>
-                <Stack space={4} w={"75%"} maxW={"300px"} mx={"auto"}>
-                    <Text fontSize={"2xl"}>Create an account</Text>
-                    <Input value={username} onChangeText={text => setUsername(text)}
-                           variant={"rounded"} placeholder={"Username"}></Input>
-                    <Input value={email} onChangeText={text => setEmail(text)}
-                           variant={"rounded"}
-                           placeholder={"Email address"}></Input>
-                    <Input value={password} type={"password"}
-                           onChangeText={text => setPassword(text)}
-                           variant={"rounded"}
-                           placeholder={"Password"}></Input>
-                    <Input value={confirmPassword} type={"password"}
-                           onChangeText={text => setConfirmPassword(text)}
-                           variant={"rounded"}
-                           placeholder={"Confirm Password"}></Input>
-                    <Button>Register</Button>
-                    <Link mx={"auto"}>Already account</Link>
-                </Stack>
+                <FormControl isInvalid={true}>
+                    <Stack space={3} w={"75%"} maxW={"300px"} mx={"auto"}>
+                        <Text mb={"10"} fontSize={"2xl"}>Create an account</Text>
+
+                        <Input value={username} onChangeText={text => setUsername(text)}
+                               variant={"rounded"} placeholder={"Username"}></Input>
+                        <FormControl.ErrorMessage
+                            ml={"5"}
+                            leftIcon={<WarningOutlineIcon size="xs"/>}>
+                            The username is empty
+                        </FormControl.ErrorMessage>
+
+                        <Input value={email} onChangeText={text => setEmail(text)}
+                               variant={"rounded"}
+                               placeholder={"Email address"}></Input>
+                        <FormControl.ErrorMessage
+                            ml={"5"}
+                            leftIcon={<WarningOutlineIcon size="xs"/>}>
+                            The email is empty
+                        </FormControl.ErrorMessage>
+
+                        <Input value={password} type={"password"}
+                               onChangeText={text => setPassword(text)}
+                               variant={"rounded"}
+                               placeholder={"Password"}></Input>
+                        <FormControl.ErrorMessage
+                            ml={"5"}
+                            leftIcon={<WarningOutlineIcon size="xs"/>}>
+                            The passwords is empty
+                        </FormControl.ErrorMessage>
+
+                        <Input value={confirmPassword} type={"password"}
+                               onChangeText={text => setConfirmPassword(text)}
+                               variant={"rounded"}
+                               placeholder={"Confirm Password"}></Input>
+                        <FormControl.ErrorMessage
+                            ml={"5"}
+                            leftIcon={<WarningOutlineIcon size="xs"/>}>
+                            The passwords do not match
+                        </FormControl.ErrorMessage>
+
+                        <Button mt={"10"}>Register</Button>
+                        <Link mx={"auto"}>Already account</Link>
+                    </Stack>
+                </FormControl>
             </Center>
         </NativeBaseProvider>
     );
