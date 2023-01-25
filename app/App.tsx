dd8f6d51a2fb0166c2201329dacda22d39cf21cf
Andres6936 <joan.buritica@artemisa.tech>
2023-01-24 20:18:47 -0500

    Added Form

--- app/App.tsx
+++ app/App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, Center, Input, Link, NativeBaseProvider, Stack, Text} from "native-base";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NativeBaseProvider>
+            <Center flex={1}>
+                <Stack space={4} w={"75%"} maxW={"300px"} mx={"auto"}>
+                    <Text fontSize={"2xl"}>Create an account</Text>
+                    <Input variant={"rounded"} placeholder={"Username"}></Input>
+                    <Input variant={"rounded"} placeholder={"Email address"}></Input>
+                    <Input variant={"rounded"} placeholder={"Password"}></Input>
+                    <Input variant={"rounded"} placeholder={"Confirm Password"}></Input>
+                    <Button>Register</Button>
+                    <Link mx={"auto"}>Already account</Link>
+                </Stack>
+            </Center>
+        </NativeBaseProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
