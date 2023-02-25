96b0b4ffa18479ecde330b53456779547c4df321
Andres6936 <andres6936@gmail.com>
2023-02-24 19:36:09 -0500

    Added Map

--- app/src/routes/Main.tsx
+++ app/src/routes/Main.tsx
@@ -1,11 +1,22 @@
-import {Box, Button, HStack, Input, Stack, Text, VStack} from "native-base";
+import {Box, Button, HStack, IconButton, Input, Stack, Text, VStack} from "native-base";
+import {MaterialIcons} from '@expo/vector-icons';
 import MapView from "react-native-maps";
 import React from "react";
 
 export function Main() {
     return (
         <Stack safeArea={true} flex={1}>
-            <MapView style={{flex: 2, width: "100%", height: "100%"}}/>
+            <Box position={"relative"} flex={2}>
+                <MapView style={{width: "100%", height: "100%"}}/>
+
+                <IconButton bg={"white"} position={"absolute"} right={15} bottom={15}
+                            icon={<MaterialIcons name="location-searching" size={24} color="black"/>}/>
+                <IconButton bg={"white"} position={"absolute"} right={15} top={15}
+                            icon={<MaterialIcons name="share" size={24} color="black"/>}/>
+                <IconButton bg={"white"} position={"absolute"} left={15} top={15}
+                            icon={<MaterialIcons name="dehaze" size={24} color="black"/>}/>
+            </Box>
+
             <VStack space={3} mx={3} py={3} flex={1}>
                 <HStack space={3}>
                     <Box flex={1} px={3} py={5} borderRadius={5} borderColor={"gray.500"} borderWidth={1}>
