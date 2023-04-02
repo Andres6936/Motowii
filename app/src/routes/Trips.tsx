ed42884994d1af53d3acf8d25638f0d7b56011ae
Andres6936 <andres6936@gmail.com>
2023-04-01 23:58:15 -0500

    Added Image

--- app/src/routes/Trips.tsx
+++ app/src/routes/Trips.tsx
@@ -1,4 +1,4 @@
-import {Divider, HStack, Text, VStack} from "native-base";
+import {Avatar, Divider, HStack, Text, VStack} from "native-base";
 
 export function Trips() {
     return (
@@ -21,10 +21,17 @@ export function Trips() {
                 </HStack>
                 <Divider my={3}/>
                 <HStack>
-                    <VStack flex={2}>
-                        <Text>Madhumd H</Text>
-                        <Text>(4.9)</Text>
-                    </VStack>
+                    <HStack space={3} flex={2}>
+                        <Avatar bg="green.500" source={{
+                            uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                        }}>
+                            AJ
+                        </Avatar>
+                        <VStack>
+                            <Text>Madhumd H</Text>
+                            <Text>(4.9)</Text>
+                        </VStack>
+                    </HStack>
                     <VStack flex={1}>
                         <Text>Final cost</Text>
                         <Text fontSize={"md"} bold>$45.65</Text>
