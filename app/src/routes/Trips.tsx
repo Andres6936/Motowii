557985a479464d134e61a2dab9b1e3f23e47e138
Andres6936 <andres6936@gmail.com>
2023-04-02 20:16:00 -0500

    Added Interaction

--- app/src/routes/Trips.tsx
+++ app/src/routes/Trips.tsx
@@ -1,9 +1,21 @@
-import {ScrollView, VStack} from "native-base";
+import {HStack, Pressable, ScrollView, Text, VStack} from "native-base";
 import {History} from "../components/History";
+import {AntDesign} from '@expo/vector-icons';
+import {DrawerActions, useNavigation} from "@react-navigation/native";
+import {DrawerNavigationProp} from "@react-navigation/drawer";
+import {RootParamList} from "../interfaces/RootParamList";
 
 export function Trips() {
+    const navigator = useNavigation<DrawerNavigationProp<RootParamList>>()
+
     return (
         <VStack bg={"gray.200"} p={3} safeArea={true} flex={1}>
+            <HStack mb={2}>
+                <Pressable onPress={() => navigator.dispatch(DrawerActions.openDrawer)}>
+                    <AntDesign name="left" size={24} color="black"/>
+                </Pressable>
+                <Text textAlign={"center"} flex={1}>History Trips</Text>
+            </HStack>
             <ScrollView>
                 <History/>
                 <History/>
