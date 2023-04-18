2c558723610932d29d64d508f4b39313325451b4
Andres6936 <andres6936@gmail.com>
2023-04-17 20:03:16 -0500

    Return Type

--- serveless/src/types/mongo.d.ts
+++ serveless/src/types/mongo.d.ts
@@ -36,6 +36,11 @@ export interface ResponseUpdateMany {
     modifiedCount: number,
 }
 
+export interface ResponseReplaceOne {
+    matchedCount: number,
+    modifiedCount: number,
+}
+
 export interface ResponseDeleteOne {
     deletedCount: number,
 }
@@ -47,3 +52,15 @@ export interface ResponseDeleteMany {
 export interface ResponseAggregate<T> {
     documents: T[]
 }
+
+export type ReturnTypeMongoResponse<Action extends TypeAction, T> =
+    Action extends "findOne" ? ResponseFindOne<T> :
+    Action extends "find" ? ResponseFind<T> :
+    Action extends "insertOne" ? ResponseInsertOne :
+    Action extends "insertMany" ? ResponseInsertMany :
+    Action extends "updateOne" ? ResponseUpdateOne :
+    Action extends "updateMany" ? ResponseUpdateMany :
+    Action extends "replaceOne" ? ResponseReplaceOne :
+    Action extends "deleteOne" ? ResponseDeleteOne :
+    Action extends "deleteMany" ? ResponseDeleteMany :
+    Action extends "aggregate" ? ResponseAggregate<T> : never
