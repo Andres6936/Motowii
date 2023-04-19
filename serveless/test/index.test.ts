7356fb521213c1c0c88bf7a80966ac1c46305bc0
Andres6936 <andres6936@gmail.com>
2023-04-18 20:05:30 -0500

    Load Enviroment Variables and Extend to 30 Seconds the Call to Function Test

--- serveless/test/index.test.ts
+++ serveless/test/index.test.ts
@@ -1,6 +1,11 @@
 import type {Event, Context, Response} from "../src/types/scaleway";
-import {expect, describe, it} from 'vitest'
+import {expect, describe, it, beforeAll} from 'vitest'
 import {handle, Payload} from "../index";
+import * as dotenv from 'dotenv';
+
+beforeAll(() => {
+    dotenv.config();
+})
 
 describe('should be create, read, update and delete user', function() {
     it('should create the user', async function() {
@@ -16,4 +21,4 @@ describe('should be create, read, update and delete user', function() {
 
         expect(response.statusCode).toEqual(200);
     });
-});
+}, 30_000);
